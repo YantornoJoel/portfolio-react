8af5ff1a50fc8c388d67c593a98abc2d0eb143fb
jyantorno <jyantorno@indec.gob.ar>
2021-10-13 01:10:26 -0300

    feat email message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'
+import emailjs from 'emailjs-com';
 import './contact.scss'
+
+const { REACT_APP_EMAIL_SERVICE, REACT_APP_EMAIL_TEMPLATE, REACT_APP_EMAIL_USERID } = process.env;
 
 export const Contact = () => {
 
-    const [message, setMessage] = useState(false);
+    const formContact = { userEmail: '', category: 'Contacto - Portfolio', emailDetails: '' };
+    const [contact, setContact] = useState(formContact);
+    const [showMessage, setShowMessage] = useState(false);
 
-    const handleSubmit = (e) => {
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setContact({ ...contact, [name]: value });
+    };
+
+    const handleSubmit = e => {
         e.preventDefault();
-        setMessage(true);
+
+        emailjs.send(REACT_APP_EMAIL_SERVICE, REACT_APP_EMAIL_TEMPLATE, contact, REACT_APP_EMAIL_USERID)
+            .then((response) => {
+                console.log('SUCCESS', response.status, response.text);
+                setContact(formContact);
+                setShowMessage(true);
+            }, (err) => {
+                console.log('FAILED...', err);
+            });
     }
 
     return (
@@ -18,10 +37,10 @@ export const Contact = () => {
             <div className="right">
                 <h2>Contacto.</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email" name="email" />
-                    <textarea placeholder="Mensaje" name="mensaje"></textarea>
+                    <input required type="text" placeholder="Email" value={contact.userEmail} name="userEmail" onChange={handleChange} />
+                    <textarea required placeholder="Mensaje" name="emailDetails" onChange={handleChange} value={contact.emailDetails}></textarea>
                     <button type="submit">Enviar</button>
-                    {message && <span>Gracias, su mensaje ha sido enviado.</span>}
+                    {showMessage && <span>Gracias, su mensaje ha sido enviado.</span>}
                 </form>
             </div>
         </div >
